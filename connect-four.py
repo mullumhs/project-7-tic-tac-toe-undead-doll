0d8c0476cb1371fe6a533a07656b374c4741d345
layliibug <140124156+undead-doll@users.noreply.github.com>
2024-04-04 14:03:18 +0000

    conect4.start

--- connect-four.py
+++ connect-four.py
@@ -1,5 +1,42 @@
-def main():
-    # Your code goes here
+board = []
 
-if __name__ == "__main__":
-    main()
+print('♥️------------᧔o᧓------------♥️')
+print('|      connect four!        |')
+
+def init_board():
+    for i in range(6):
+        board.append([' - ',' - ',' - ',' - ',' - ',' - ',' - '])
+
+def print_board():
+    print('♥️---------------------------♥️')
+    print('| 1 | 2 | 3 | 4 | 5 | 6 | 7 |')
+    print('♥️---------------------------♥️')
+    for row in board:
+        print('', end='|')
+        for col in row:
+            print(col, end='|')
+        print() 
+
+playerCounter = 1
+init_board()
+print_board()
+
+while True:
+
+    token = ' X '
+    if playerCounter % 2 == 0:
+        token = ' O '
+
+    print('♥️---------------------------♥️')
+    print('')
+    choice =int(input("choose column (1-7): "))
+    print('')
+    choice -= 1
+
+    for i in range (5, -1, -1):
+        if board[i][choice] == ' - ':
+            board[i][choice] = token
+            playerCounter += 1
+            break
+
+    print_board()
